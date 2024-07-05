77101e821932a01ed892fba0e9e55d2537466034
Michael Stepanischevskiy <mihail.mike@mail.ru>
2024-07-05 13:24:37 +0300

    err4 commit

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(1000)
-	for i := 0; i < 1000; i++ {
+	wg.Add(100000)
+	for i := 0; i < 100000; i++ {
 		go func() {
 			fmt.Println(i)
 			wg.Done()
