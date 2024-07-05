ac94863a455c955ddbeeb518d3dc1cecb46db66f
Michael Stepanischevskiy <mihail.mike@mail.ru>
2024-07-05 13:31:17 +0300

    newwf commit

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(100000)
-	for i := 0; i < 100000; i++ {
-		go func() {
-			fmt.Println(i)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
+
 }
 
 func MaxInt(a, b int) int {
